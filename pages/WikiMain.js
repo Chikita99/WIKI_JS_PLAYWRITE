d22694dfaca10c57f753162015406333d35f9b5b
Chikita99 <9chikita9@gmail.com>
2024-09-25 14:23:13 +0300

    Main Title TC's added

--- pages/WikiMain.js
+++ pages/WikiMain.js
@@ -5,6 +5,8 @@ export class WikiMain {
 
     mainTitle = '#Welcome_to_Wikipedia'
     mainSubtitle = '#mp-free'
+    articleCountText = '#articlecount'
+    //articleCountNumber = "//div[@id='articlecount']/a[@title='${specialTitle}']";
 
     async getMainTitle() {
         const title = await (
@@ -18,5 +20,18 @@ export class WikiMain {
             await this.page.locator(this.mainSubtitle)
         ).textContent()
         return subTitle
+    }
+
+    async getCounterText() {
+        const countArticle = await (
+            await this.page.locator(this.articleCountText)
+        ).textContent()
+        return countArticle
+    }
+
+    async getCounterLink(specialTitle) {
+        const xpath = `//div[@id='articlecount']/a[@title='${specialTitle}']`
+        const countLink = await this.page.locator(`xpath=${xpath}`)
+        return countLink
     }
 }
--- tests/wiki.spec.js
+++ tests/wiki.spec.js
@@ -1,14 +1,32 @@
 import { test } from '../fixture/baseFixture'
 import { expect } from '@playwright/test'
 
-test('main Title check', async ({ wikiMain, page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('wiki/Main_Page')
+})
+
+test('main Title check', async ({ wikiMain }) => {
     expect(await wikiMain.getMainTitle()).toBe('Welcome to Wikipedia')
 })
 
-test('main Subtitle check', async ({ wikiMain, page }) => {
-    await page.goto('wiki/Main_Page')
+test('main Subtitle check', async ({ wikiMain }) => {
     expect(await wikiMain.getMainSubtitle()).toBe(
         'the free encyclopedia that anyone can edit.'
     )
+})
+
+test('article counter text check', async ({ wikiMain }) => {
+    expect(
+        (await wikiMain.getCounterText()).includes('articles in English')
+    ).toBeTruthy()
+})
+
+test('article counter link check - Statistics', async ({ wikiMain }) => {
+    const link = await wikiMain.getCounterLink('Special:Statistics')
+    expect(await link.getAttribute('href')).toBe('/wiki/Special:Statistics')
+})
+
+test('article counter link check - Language', async ({ wikiMain }) => {
+    const link = await wikiMain.getCounterLink('English language')
+    expect(await link.getAttribute('href')).toBe('/wiki/English_language')
 })
